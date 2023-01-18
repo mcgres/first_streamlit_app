95d291c9ff43275a5b0d1e77841f5bc03d1df6b0
mcgres <122609118+mcgres@users.noreply.github.com>
2023-01-18 12:00:39 +0000

    Update streamlit_app.py
    added smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
